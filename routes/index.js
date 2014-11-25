edac425c0988037f0b22dc1f42a4c625dd95025a
shanghongliang <shanghongliang@tizi.com>
2014-11-25 14:59:55 +0800

    1.微信详细页面的单页面抓取

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+﻿var myUtil = require('../myUtil.js');
+var $ = require('jQuery');
+var http = require('http');
+var url="http://mp.weixin.qq.com/s?__biz=MjM5NTY2NjY4Nw==&mid=200882176&idx=1&sn=946fd4ecec5830584271035a7eada7cb&scene=4#wechat_redirect";
+//var url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5NTY2NjY4Nw==#wechat_webview_type=1&wechat_redirect";
+//var url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5NTY2NjY4Nw==&uin=ODM2NDE0NTIx&key=63ae54cc90a5df05f399484d726abbfb35e0faacff1bed1c1b6366d0dfd348c0018f8c580b266c5487e3ed53349bfbc5&devicetype=android-18&version=26000036&lang=zh_CN&pass_ticket=xZehGaW7aQpjnEjPKLNj1Wn0FV6PrueH9ZqrwEz43sYYtAvYNnLAS5iLdWO%2BSB0z";
+http.createServer(function(request, response) {   
+	 myUtil.get(url,function(content,status){
+		var movie={}
+		movie.name = $(content).find('span[property="v:itemreviewed"]').text();
+		movie.director = $(content).find('#info span:nth-child(1) a').text();
+		//console.log(movie);
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write(content);
+		response.end();
+	});
+}).listen(9999); 
+ console.log("Server has started."); 
+
+
+
+
