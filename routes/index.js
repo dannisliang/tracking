21d2a813710c89640f272d291792c0580affdae6
shanghongliang <shanghongliang@tizi.com>
2014-11-26 10:49:52 +0800

    1.修改依赖包

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 ﻿var myUtil = require('../myUtil.js');
 var $ = require('jQuery');
 var http = require('http');
-var url="http://mp.weixin.qq.com/s?__biz=MjM5NTY2NjY4Nw==&mid=200882176&idx=1&sn=946fd4ecec5830584271035a7eada7cb&scene=4#wechat_redirect";
+var url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5NTY2NjY4Nw&key=b0212c493356f8b0ad417e39d44eee253901cbd3300c0fa382c22a21fe774ca276cdd2a341f55257c1a31606bd54fcc0==#wechat_redirect";
 //var url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5NTY2NjY4Nw==#wechat_webview_type=1&wechat_redirect";
 //var url="http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MjM5NTY2NjY4Nw==&uin=ODM2NDE0NTIx&key=63ae54cc90a5df05f399484d726abbfb35e0faacff1bed1c1b6366d0dfd348c0018f8c580b266c5487e3ed53349bfbc5&devicetype=android-18&version=26000036&lang=zh_CN&pass_ticket=xZehGaW7aQpjnEjPKLNj1Wn0FV6PrueH9ZqrwEz43sYYtAvYNnLAS5iLdWO%2BSB0z";
 http.createServer(function(request, response) {   
