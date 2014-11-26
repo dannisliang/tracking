a04e3ea5f3d5baadb2a07431653b7cd4a8acad76
shanghongliang <shanghongliang@tizi.com>
2014-11-26 16:47:17 +0800

    1111

---
+++ server/user.js
@@ -0,0 +1,9 @@
+﻿var mysql = require('mysql');
+var db = require('../util/db.js');
+//获取微信账号列表
+var getWechatUsers=function(){
+	var sql="select * from wechats;";
+	var users=db.getList(sql,null);
+	console.log(users);
+}
+exports.getWechatUsers=getWechatUsers;
---
+++ util/db.js
@@ -0,0 +1,41 @@
+﻿var mysql = require('mysql');
+//数据库连接配置
+var conn = mysql.createConnection({
+    host: 'localhost',//数据库host
+    user: 'root',//数据库用户名
+    password: '',//数据库密码
+    database:'weixin',//数据库名
+    port: 3306//数据端口号
+});
+
+
+//获取单个对象信息
+var getItem=function(sql,params){
+	exports.query(sql,params);
+}
+//获取列表信息
+var getList=function(sql,params){
+	return query(sql,params);
+}
+//添加单个信息
+var addItem=function(){
+	exports.query(sql,params);
+}
+//删除单个信息
+var delItem=function(){
+	exports.query(sql,params);
+}
+//query查询操作
+var query=function(sql,params){
+	conn.connect();
+	console.log(sql);
+	conn.query(sql,params, function(err, rows, fields) {
+		if (err) throw err;
+		console.log("db:query");
+		console.log(rows);
+		return rows;
+	});
+	conn.end();
+}
+
+exports.getList=getList;
